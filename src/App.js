de62dd3cdaadc90966b5017e12efa77df3ab391b
cedric-winbush <cawinbushjr@gmail.com>
2020-09-09 13:52:19 -0500

    grabs token from spotify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import { getTokenFromUrl } from './utils/spotify'
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+    console.log("I have a token: ", token)
+
+    
+    
+  }, []);
+  
   return (
     <div className="app">
       <Login />
-      {/* Spotify Logo */}
-      {/* Login with spotify buton */}
     </div>
   );
 }
