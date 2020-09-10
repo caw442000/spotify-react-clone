efad31bd3daad56a9623853672b3c4233d7e39d1
cedric-winbush <cawinbushjr@gmail.com>
2020-09-10 15:01:53 -0500

    grabs token from spotify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
-import { getTokenFromUrl } from './utils/spotify'
+import { getTokenFromUrl } from "./utils/spotify";
 
 const App = () => {
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
-    console.log("I have a token: ", token)
+    const _token = hash.access_token;
+    console.log("I have a token: ", _token);
 
-    
-    
+    if (_token) {
+      setToken(_token);
+    }
   }, []);
-  
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1> I am logged in </h1> : <Login />}</div>
   );
-}
+};
 
 export default App;
