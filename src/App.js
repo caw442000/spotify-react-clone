ab162d6ea4a35823cefbe3b15df0359e87ece92f
cedric-winbush <cawinbushjr@gmail.com>
2020-09-13 17:07:23 -0500

    adds context api with reducer setup

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromUrl } from "./utils/spotify";
 import SpotifyWebApi from "spotify-web-api-js"
+import Player from "./components/Player";
+import {useStateProviderValue } from './reducer/StateProvider';
 
 const spotify = new SpotifyWebApi();
 
 
 const App = () => {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{ user, token}, dispatch] = useStateProviderValue();
+
+  // const [{ user}] this is like saying const [StateProvider]
+  // to get to user it is StateProvider.user
+
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    console.log("I have a token: ", _token);
 
     if (_token) {
-      setToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
 
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log("user", user)
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+
+        })
       })
       
     }
@@ -30,8 +45,12 @@ const App = () => {
 
   }, []);
 
+
+  console.log("user", user);
+  console.log("I have a token: ", token);
+
   return (
-    <div className="app">{token ? <h1> I am logged in </h1> : <Login />}</div>
+    <div className="app">{token ? <Player /> : <Login />}</div>
   );
 };
 
---
+++ src/reducer/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  //remove after developing
+  token: process.env.REACT_APP_SPOTIFY_TEMP_TOKEN,
+  // token: null,
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  //Action -> type, [payload]
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
