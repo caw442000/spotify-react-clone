8dda5d1c55da66c2ce9df37ada930832f59aa9d0
cedric-winbush <cawinbushjr@gmail.com>
2020-09-15 10:39:42 -0500

    styles footer

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromUrl } from "./utils/spotify";
-import SpotifyWebApi from "spotify-web-api-js"
+import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./components/Player";
-import {useStateProviderValue } from './reducer/StateProvider';
+import { useStateProviderValue } from "./reducer/StateProvider";
 
 const spotify = new SpotifyWebApi();
 
-
 const App = () => {
   // const [token, setToken] = useState(null);
-  const [{ user, token}, dispatch] = useStateProviderValue();
+  const [{ user, token }, dispatch] = useStateProviderValue();
 
   // const [{ user}] this is like saying const [StateProvider]
   // to get to user it is StateProvider.user
-
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -23,35 +21,32 @@ const App = () => {
     const _token = hash.access_token;
 
     if (_token) {
-
       dispatch({
         type: "SET_TOKEN",
-        token: _token
-      })
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
-
+      spotify.getMe().then((user) => {
         dispatch({
-          type: 'SET_USER',
+          type: "SET_USER",
           user: user,
-
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
         })
       })
-      
     }
-
-
   }, []);
-
 
   console.log("user", user);
   console.log("I have a token: ", token);
 
-  return (
-    <div className="app">{token ? <Player /> : <Login />}</div>
-  );
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 };
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import {Home, Search, LibraryMusic} from '@material-ui/icons';
+import { useStateProviderValue } from "../reducer/StateProvider";
 
 const Sidebar = () => {
+  const[{playlists }, dispatch] = useStateProviderValue()
   return (
     <div className="sidebar">
       <img className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOption title = "Home"/>
-      <SidebarOption title = "Search"/>
-      <SidebarOption title = "Your Library"/>
+      <SidebarOption title = "Home" Icon = {Home}/>
+      <SidebarOption title = "Search" Icon = {Search}/>
+      <SidebarOption title = "Your Library" Icon = {LibraryMusic}/>
+      <br/>
+      <strong className="sidebar__title"> PLAYLISTS</strong>
+      <hr/>
+      {playlists?.items?.map(playlist => (
+
+      <SidebarOption title = {playlist.name} />
+      )) }
+      
     </div>
   );
 };
